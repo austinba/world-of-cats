fda547a85ba8f78f86497d625e9a49a4a940dd30
Austin Baltes <austinba@gmail.com>
2018-02-17 09:03:11 -0800

    added infrastructure for routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import configureStore from './store';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/hi" component={App} />
+    </Router>
+  </Provider>
+  , document.getElementById('root'));
+
 registerServiceWorker();
