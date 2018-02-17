a7a273984668ef630427c6881e7fae93bae3618b
Austin Baltes <austinba@gmail.com>
2018-02-17 09:50:07 -0800

    measuring image size

--- src/components/cat-box.js
+++ src/components/cat-box.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Dimensions from 'react-dimensions';
 
 class CatBox extends React.Component {
 
   render() {
+    const Contents = Dimensions({elementResize: true})(dimensions =>
+      <div>
+        <div><img src={this.props.image} className="cat-image" /></div>
+        <div className="cat-text">{this.props.fact}</div>
+        <div>{dimensions.containerHeight}, {dimensions.containerWidth}</div>
+      </div>
+    );
     return <div className="cat-box">
-      <div><img src={this.props.image} className="cat-image" /></div>
-      <div className="cat-text">{this.props.fact}</div>
+      <Contents />
     </div>
   }
 }
