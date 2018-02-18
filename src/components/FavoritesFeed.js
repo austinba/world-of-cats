5fe26f60fb4b99224d970ae8faa0f94bdf5cafb7
Austin Baltes <austinba@gmail.com>
2018-02-17 17:47:21 -0800

    stylistic update

--- src/components/FavoritesFeed.js
+++ src/components/FavoritesFeed.js
@@ -6,7 +6,11 @@ import * as R from 'ramda';
 const FavoritesFeed = (props) => {
   const {cats, favorites} = props;
   const favoriteCats = R.values(R.pickAll(R.values(favorites || {}), cats.data));
-  if(favoriteCats.length === 0) return <div className="message">You haven't selected any favorites yet!</div>;
+  if(favoriteCats.length === 0) {
+    return (
+      <div className="message">You haven't selected any favorites yet!</div>
+    );
+  }
   return (
     <Feed cats={favoriteCats} />
   );
